f42db7612d758af525505dd46c8ae741b36bfb29
Use your GitHub username <jer14002@byui.edu>
2022-05-12 17:24:11 -0600

    changes

---
+++ chamber/js/main.js
@@ -0,0 +1,3 @@
+document.querySelector('#year').innerHTML = (new Date().getFullYear());
+
+document.querySelector('#lastUpdate').innerHTML = ` - Mariah Wedel - Idaho - Last Updated: ${document.lastModified}`;
