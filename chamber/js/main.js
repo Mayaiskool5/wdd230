5ec5edc411f7a7249d9e764ca7be6137069b8350
Use your GitHub username <jer14002@byui.edu>
2022-05-12 21:42:04 -0600

    changes

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,3 +1,9 @@
-document.querySelector('#year').innerHTML = (new Date().getFullYear());
+
+const datefield = document.querySelector("#year");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
 
 document.querySelector('#lastUpdate').innerHTML = ` - Mariah Wedel - Idaho - Last Updated: ${document.lastModified}`;
