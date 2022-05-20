da163bc55647d64ac8a0eb690a27406cad408eed
Use your GitHub username <jer14002@byui.edu>
2022-05-19 23:27:27 -0600

    index

--- bom/bom.js
+++ bom/bom.js
@@ -1,62 +1,62 @@
 window.addEventListener('load', () => {
-    const form = document.querySelector("#new-chapter-form");
-    const input = document.querySelector("#new-chapter-input");
-    const list_el = document.querySelector("#chapters");
+    const form = document.querySelector("#new-chapter");
+    const input = document.querySelector("#favChap-input");
+    const list = document.querySelector("#chapters");
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
         const chapter = input.value;
 
-        const chapter_el = document.createElement('div');
-        chapter_el.classList.add('chapter');
+        const chapter_in = document.createElement('div');
+        chapter_in.classList.add('chapter');
 
-        const chapter_content_el = document.createElement('div');
-        chapter_content_el.classList.add('content');
+        const chapter_content = document.createElement('div');
+        chapter_content.classList.add('content');
 
-        chapter_el.appendChild(chapter_content_el);
+        chapter_in.appendChild(chapter_content);
 
-        const chapter_input_el = document.createElement('input');
-        chapter_input_el.classList.add('text');
-        chapter_input_el.type = 'text';
-        chapter_input_el.value = chapter;
-        chapter_input_el.setAttribute('readonly', 'readonly');
+        const chapter_input = document.createElement('input');
+        chapter_input.classList.add('text');
+        chapter_input.type = 'text';
+        chapter_input.value = chapter;
+        chapter_input.setAttribute('readonly', 'readonly');
 
-        chapter_content_el.appendChild(chapter_input_el);
+        chapter_content.appendChild(chapter_input);
 
-        const chapter_actions_el = document.createElement('div');
-        chapter_actions_el.classList.add('actions');
+        const chapter_actions = document.createElement('div');
+        chapter_actions.classList.add('actions');
 
-        const chapter_edit_el = document.createElement('button');
-        chapter_edit_el.classList.add('edit');
-        chapter_edit_el.innerText = 'Edit';
+        const chapter_edit = document.createElement('button');
+        chapter_edit.classList.add('edit');
+        chapter_edit.innerText = 'Edit';
 
-        const chapter_delete_el = document.createElement('button');
-        chapter_delete_el.classList.add('delete');
-        chapter_delete_el.innerText = 'Delete';
+        const chapter_delete = document.createElement('button');
+        chapter_delete.classList.add('delete');
+        chapter_delete.innerText = 'Delete';
 
-        chapter_actions_el.appendChild(chapter_edit_el);
-        chapter_actions_el.appendChild(chapter_delete_el);
+        chapter_actions.appendChild(chapter_edit);
+        chapter_actions.appendChild(chapter_delete);
 
-        chapter_el.appendChild(chapter_actions_el);
+        chapter_in.appendChild(chapter_actions);
 
-        list_el.appendChild(chapter_el);
+        list.appendChild(chapter_in);
 
         input.value = '';
 
-        chapter_edit_el.addEventListener('click', (e) => {
-            if (chapter_edit_el.innerText.toLowerCase() == "edit") {
-                chapter_edit_el.innerText = "Save";
-                chapter_input_el.removeAttribute("readonly");
-                chapter_input_el.focus();
+        chapter_edit.addEventListener('click', (e) => {
+            if (chapter_edit.innerText.toLowerCase() == "edit") {
+                chapter_edit.innerText = "Save";
+                chapter_input.removeAttribute("readonly");
+                chapter_input.focus();
             } else {
-                chapter_edit_el.innerText = "Edit";
-                chapter_input_el.setAttribute("readonly", "readonly");
+                chapter_edit.innerText = "Edit";
+                chapter_input.setAttribute("readonly", "readonly");
             }
         });
 
-        chapter_delete_el.addEventListener('click', (e) => {
-            list_el.removeChild(chapter_el);
+        chapter_delete.addEventListener('click', (e) => {
+            list.removeChild(chapter_in);
         });
     });
 });
