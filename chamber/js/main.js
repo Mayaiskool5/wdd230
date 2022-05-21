317c673180de2580c516f0f50aaae12c049e4582
Use your GitHub username <jer14002@byui.edu>
2022-05-20 22:59:09 -0600

    changes

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -35,7 +35,7 @@ document.getElementById( "lastupdate" ).textContent = lastModification ;
 document.getElementById( "year" ).textContent = year ;
 document.getElementById( "date" ).textContent = `${dayName}, ${day} ${month} ${year}`
 
-if (dayName == "Monday" || dayName == "Friday") {
+if (dayName == "Monday" || dayName == "Tuesday") {
     document.querySelector("#banner").textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 }
     
