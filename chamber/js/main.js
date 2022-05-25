0c633a8c5ff1c0e6bc3f0087a673c0df2d3d757f
Use your GitHub username <jer14002@byui.edu>
2022-05-24 21:17:46 -0600

    weather

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -38,4 +38,3 @@ document.getElementById( "date" ).textContent = `${dayName}, ${day} ${month} ${y
 if (dayName == "Monday" || dayName == "Tuesday") {
     document.querySelector("#banner").textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 }
-    
