24c112896395877c5a0eea8d1f4989f281217f3b
Use your GitHub username <jer14002@byui.edu>
2022-05-25 18:38:36 -0600

    weather

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,10 +1,15 @@
 function calculateWindChill() {
     var temp = document.getElementById("Temp").value;
     var wSpeed = document.getElementById("windSpeed").value;
-    var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+    if ((temp <= 50) && (wSpeed > 3)) {
+        var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
-    windChill= Math.round(windChill);
-    document.getElementById("windChill").innerHTML= windChill;
+        windChill= Math.round(windChill);
+        document.getElementById("windChill").innerHTML= windChill;
+    } else {
+        document.getElementById('windChill').innerHTML= "N/A";
+    }
+
 }
 
 document.querySelector('#button').onclick = calculateWindChill;
