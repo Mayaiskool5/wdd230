6ed081bde4df1547ce9a53ff3e0bbdb4ab2b5121
Use your GitHub username <jer14002@byui.edu>
2022-07-14 18:32:24 -0600

    temple site

---
+++ temple-inn-suites/js/main.js
@@ -0,0 +1,44 @@
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+] ;
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+] ;
+
+let currentDate = new Date() ;
+let lastModification = document.lastModified ;
+let day = currentDate.getDay() ;
+let dayName = days[ day ] ;
+let month = months[ currentDate.getMonth() ] ;
+let year = currentDate.getFullYear() ;
+
+document.getElementById( "lastupdate" ).textContent = lastModification ;
+document.getElementById( "year" ).textContent = year ;
+document.getElementById( "date" ).textContent = `${dayName}, ${day} ${month} ${year}`
+
+if (dayName == "Monday" || dayName == "Tuesday") {
+    document.querySelector("#banner").textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
+
+var dateNow = Date.now();
+var hidden = document.querySelector("#hiddenField");
+hidden.value = dateNow;
